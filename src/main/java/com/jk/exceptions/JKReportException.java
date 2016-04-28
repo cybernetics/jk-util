fd87e2ab8d9c91a0b3ec0bab6dfe368222a4da4f
Jalal Kiswani <kiswanij@yahoo.com>
2016-04-29 01:03:32 +0300

    c

---
+++ src/main/java/com/jk/exceptions/JKReportException.java
@@ -0,0 +1,30 @@
+package com.jk.exceptions;
+
+public class JKReportException extends Exception {
+
+	public JKReportException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKReportException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKReportException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKReportException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKReportException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/jk/exceptions/JKSecurityException.java
@@ -0,0 +1,30 @@
+package com.jk.exceptions;
+
+public class JKSecurityException extends JKException {
+
+	public JKSecurityException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKSecurityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKSecurityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKSecurityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JKSecurityException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
