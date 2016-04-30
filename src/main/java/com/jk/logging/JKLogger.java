b962e0f9f2137c62b590e211bed91481a87e76c3
Your Name <you@example.com>
2016-05-01 01:56:15 +0200

    enhance the API and caching the loggers

--- src/main/java/com/jk/logging/JKLogger.java
+++ src/main/java/com/jk/logging/JKLogger.java
@@ -2,30 +2,99 @@ package com.jk.logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
 
 
 public class JKLogger {
 	private Logger logger = null;
+	private String name;
 	
 	public JKLogger(String name) {
 		logger = LoggerFactory.getLogger(name);
+		this.name = name;
 	}
 	
-	public void info(String msg){
-		logger.info(msg);
+	public void info(String... msgs){
+		if(msgs == null){
+			return ;
+		}
+		for (String msg : msgs) {
+			logger.info(msg);
+		}
 	}
 	
-	public void debug(String msg){
-		logger.debug(msg);
+	public void debug(String... msgs){
+		if(msgs == null){
+			return ;
+		}
+		for (String msg : msgs) {
+			logger.debug(msg);
+		}
 	}
 	
-	public void trace(String msg){
-		logger.trace(msg);
+	public void trace(String... msgs){
+		if(msgs == null){
+			return ;
+		}
+		for (String msg : msgs) {
+			logger.trace(msg);
+		}
 	}
 	
-	public void error(String msg){
-		logger.error(msg);
+	public void error(String... msgs){
+		if(msgs == null){
+			return ;
+		}
+		for (String msg : msgs) {
+			logger.error(msg);
+		}
 	}
 	
+	public void warn(String... msgs){
+		if(msgs == null){
+			return ;
+		}
+		for (String msg : msgs) {
+			logger.warn(msg);
+		}
+	}
+	
+	public void error(Throwable t){
+		if(t == null){
+			return ;
+		}
+		error(t.getMessage() , t);
+	}
+	
+	public void error(String msg,Throwable t){
+		if(t == null){
+			return ;
+		}
+		logger.error(msg, t);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isDebugEnabled() {
+		return logger.isDebugEnabled();
+	}
+
+	public boolean isErrorEnabled() {
+		return logger.isErrorEnabled();
+	}
+
+	public boolean isInfoEnabled() {
+		return logger.isInfoEnabled();
+	}
+	
+	public boolean isTraceEnabled() {
+		return logger.isTraceEnabled();
+	}
+
+	public boolean isWarnEnabled() {
+		return logger.isWarnEnabled();
+	}
 	
 }
--- src/main/java/com/jk/logging/JKLoggerFactory.java
+++ src/main/java/com/jk/logging/JKLoggerFactory.java
@@ -1,14 +1,26 @@
 package com.jk.logging;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 
 public class JKLoggerFactory {
+	private static Map<Class, JKLogger> loggers = new Hashtable<Class, JKLogger>();
+	private static Object lock = new Object();
 
 	private JKLoggerFactory(){
 		
 	}
 	
-	public static JKLogger getLogger(Class class1 ){
-		return new JKLogger(class1.getName());
+	public static JKLogger getLogger(Class clazz ){
+		if(loggers.get(clazz) == null){
+			synchronized (lock) {
+				if(loggers.get(clazz) == null){
+					loggers.put(clazz, new JKLogger(clazz.getName()));
+				}
+			}
+		}
+		return loggers.get(clazz);
 	} 
 	
 	public static void main(String[] args) {
